305fa7bdfb7b8da6055585dccb3b1dc672489093
Patrick O'Neill <paddy.oneill93@gmail.com>
2021-12-01 18:06:17 +0000

    dwm: Update config.h
    * Change borderpx to 3
    * Add hack to font list
    * Change colours
    * Change tag names
    * Change modmask to super key

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -1,25 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+
+/* bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+/* fonts */
+static const char *fonts[]          = {
+  "hack:size=10:style=bold",
+  "monospace:size=10"
+};
+static const char dmenufont[]       = "hack:size=10:style=bold";
+
+/* colours */
+static const char col_dark[]  = "#1b3245";
+static const char col_light[] = "#18a8f0";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_light, col_dark, col_light },
+	[SchemeSel]  = { col_dark, col_light,  col_dark },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "DEV", "WEB", "CHAT", "MON" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,7 +51,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark, "-nf", col_light, "-sb", col_light, "-sf", col_dark, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
